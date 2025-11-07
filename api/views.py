65c456732a139e4ecd97b08b68ef53afcb56b376
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2025-11-07 14:28:01 -0300

    feat: Cria a view para testar a comunicação com o servidor

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def health_check():
+    return JsonResponse({"Success":"All systems normal"}, status=200)
