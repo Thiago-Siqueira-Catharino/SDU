322352dbd0dc3c4380ac2dee5b76ee592c55ed10
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2025-11-14 15:47:14 -0300

    feat(api): add feature to upload and save Diagnostics
    endpoint: hosting_url/api/upload/diagnostic
    method: POST
    request params:
    - cpf (11-character integer)
    - cid (4-character string)
    - file (file object to upload)
    - exames (JSON list with Exam object ids)

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.health_check),
     path("upload/exam", views.upload_exam,),
     path("download/exam", views.download_exam),
-    path("search/exam", views.get_exams)
+    path("search/exam", views.get_exams),
+    path("upload/diagnostic", views.upload_diagnosis),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from . import utils as u
 from . import models
+import re, json
 # Create your views here.
 def health_check(request):
     return JsonResponse({
@@ -16,7 +17,6 @@ def upload_exam(request):
     if error: return error
     
     file = request.FILES.get("file")
-
     if not file:
         return JsonResponse({
             "status":"error",
@@ -64,7 +64,7 @@ def download_exam(request):
     if not obj:
         return JsonResponse({
             "status":"error",
-            "message":f"object with id = {id} not found",
+            "message":f"Exam object with id = {id} not found",
             }, status = 404)
     
     return JsonResponse({
@@ -103,3 +103,68 @@ def get_exams(request):
         "message":"Valid objects found in database",
         "exames":list(exames)
         })
+
+@csrf_exempt
+def upload_diagnosis(request):
+    error = u.handle_request_method(request, 'POST')
+    if error: return error
+
+    file = request.FILES.get("file")
+    if not file:
+        return JsonResponse({
+            "status":"error",
+            "message":"No file was uploaded"
+            }, status = 400)
+    
+    try:
+        path = u.upload_s3(file_obj=file)
+    except Exception as e:
+        return JsonResponse({
+            "status":"error",
+            "message":"something went wrong while uploading the file",
+            "error":str(e),
+            }, status = 500)
+    
+    cpf = request.POST.get("cpf")
+    cid = request.POST.get("cid")
+
+    exames_raw = request.POST.get("exames")
+    if not exames_raw:
+        return JsonResponse({
+            "status":"error",
+            "message":"missing param: exames",
+            }, status = 400)
+
+    try:
+        exames_list = json.loads(exames_raw)
+    except Exception as e:
+        return JsonResponse({
+            "status":"error",
+            "message":"malformed param: exames",
+            "error":str(e),
+            }, status = 400)
+    
+    exames_objt = []
+    for exame in exames_list:
+        obj = models.Exame.objects.filter(id=exame).first()
+        if not obj:
+            return JsonResponse({
+            "status":"error",
+            "message":f"Exam object with id = {exame} not found",
+            }, status = 400)
+        
+        exames_objt.append(obj)
+        
+    new_file = models.Diagnostico(
+        cpf = cpf,
+        cid = cid,
+        path = path,
+    )
+    
+    new_file.exames.set(exames_objt)
+    new_file.save()
+
+    return JsonResponse({
+        "status":"success",
+        "message":"file saved successfully"
+        }, status=200)
